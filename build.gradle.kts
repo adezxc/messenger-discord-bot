69b3239fec447ff845938a90f817dbaee043a8fb
adezxc <adam@jasinski.lt>
2024-09-07 12:29:49 +0300

    Add SqlDelight to project, but maybe I don't need that?

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,30 @@
 plugins {
     kotlin("jvm") version "2.0.0"
     kotlin("plugin.serialization") version "2.0.0"
+    id("app.cash.sqldelight") version "2.0.2"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    google()
     mavenCentral()
+}
+
+sqldelight {
+    databases {
+        create("Database") {
+            packageName.set("com.example")
+        }
+    }
 }
 
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
+    implementation("app.cash.sqldelight:sqlite-driver:2.0.2")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 package org.example
 
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import com.example.Database
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromStream
@@ -8,6 +11,9 @@ import java.io.File
 
 @kotlinx.serialization.ExperimentalSerializationApi
 fun main() {
+
+    val driver: SqlDriver = JdbcSqliteDriver("jdbc:sqlite:test.db")
+    Database.Schema.create(driver)
     val format = Json { ignoreUnknownKeys = true; prettyPrint = true; explicitNulls = true}
 
     val test = """
